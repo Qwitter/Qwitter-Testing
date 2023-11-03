8e48cb23efd75816653e81a96d5dc8531c949bd3
[Marwan Emad] <[marwanemad910@gmail.com]>
2023-11-03 22:42:37 +0200

    chore: Environment setup

---
+++ Web/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  // there is a line also need in support/e2e.js
+  //reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      
+      //for generating html reports
+      //require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
