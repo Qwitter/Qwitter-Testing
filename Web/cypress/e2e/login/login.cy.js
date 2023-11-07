ab6fefecfb1875fdc00b9d5c47192170ef82dfe5
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2023-11-08 00:13:17 +0200

    test(login): add new tests
    Add new tests for login page:
    - shows signup button
    - shows forget password button
    - requires valid email
    - requires password
    - shows google signin button

--- Web/cypress/e2e/login/login.cy.js
+++ Web/cypress/e2e/login/login.cy.js
@@ -6,16 +6,49 @@ describe('login page', () => {
             globalThis.data = data
         })
     })
-
+    
     beforeEach('navigate to login page and input field should be empty', () => {
         cy.visit('')
         LoginPagePo.emailInputFieldEmailScreen.should('be.empty')
         LoginPagePo.nextButtonEmailScreen.should('be.disabled')
     })
 
+    it('shows signup button', () => {
+        LoginPagePo.signupButtonEmailScreen
+        .should('contain.text', "Sign up")
+        .should('be.visible')
+        LoginPagePo.emailInputFieldEmailScreen.type(data.loginPage.dummyEmail)
+        LoginPagePo.nextButtonEmailScreen.click()
+        LoginPagePo.signupButtonPasswordScreen
+        .should('contain.text', "Sign up")
+        .should('be.visible')
+    })
+
+    it('shows forgot password button', () => {
+        LoginPagePo.forgotPasswordButtonEmailScreen
+        .should('be.visible')
+        .invoke('text')
+        .then((text) => {
+            expect(text.toLowerCase()).to.include('forgot password')
+        })
+        LoginPagePo.emailInputFieldEmailScreen.type(data.loginPage.dummyEmail)
+        LoginPagePo.nextButtonEmailScreen.click()
+        LoginPagePo.forgotPasswordButtonPasswordScreen
+        .should('be.visible')
+        .invoke('text')
+        .then((text) => {
+            expect(text.toLowerCase()).to.include('forgot password')
+        })
+    })
+
     it('requires email', () => {
         LoginPagePo.emailInputFieldEmailScreen.type(data.loginPage.dummyEmail)
         LoginPagePo.nextButtonEmailScreen.should('be.enabled')
+    })
+    
+    it('requires valid email', () => {
+        LoginPagePo.emailInputFieldEmailScreen.type(data.loginPage.invalidEmail)
+        LoginPagePo.nextButtonEmailScreen.should('be.disabled')
     })
 
     it('proceeds to password screen with valid email', () => {
@@ -24,6 +57,25 @@ describe('login page', () => {
         LoginPagePo.titlePasswordScreen.should('contain.text', 'Enter your password')
         LoginPagePo.emailInputFieldPasswordScreen.should('have.value', data.loginPage.dummyEmail)
         LoginPagePo.passwordInputFieldPasswordScreen.should('be.empty')
-        LoginPagePo.logInButtonPasswordScreen.should('be.disabled')
+        LoginPagePo.loginButtonPasswordScreen.should('be.disabled')
+    })
+
+    it('requires password', () => {
+        LoginPagePo.emailInputFieldEmailScreen.type(data.loginPage.dummyEmail)
+        LoginPagePo.nextButtonEmailScreen.click()
+        LoginPagePo.passwordInputFieldPasswordScreen.type(data.loginPage.dummyPassword)
+        LoginPagePo.loginButtonPasswordScreen.should('be.enabled')
+    })
+
+    // fails for now ???
+    it.skip('requires valid password', () => {
+        LoginPagePo.emailInputFieldEmailScreen.type(data.loginPage.dummyEmail)
+        LoginPagePo.nextButtonEmailScreen.click()
+        LoginPagePo.passwordInputFieldPasswordScreen.type(data.loginPage.invalidPassword)
+        LoginPagePo.loginButtonPasswordScreen.should('be.disabled')
+    })
+
+    it('shows google sign in button', () => {
+        LoginPagePo.googleSigninButton.should('be.visible')
     })
 })
--- Web/cypress/support/page-objects/LoginPagePo.js
+++ Web/cypress/support/page-objects/LoginPagePo.js
@@ -4,7 +4,12 @@ class LoginPagePo {
     get titlePasswordScreen() { return cy.get("p[class='my-5 text-3xl font-bold break-words text-start h-[40px] w-[440px]']") }
     get emailInputFieldPasswordScreen() { return cy.get("body > div:nth-child(5) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) input") }
     get passwordInputFieldPasswordScreen() { return cy.get("input[name='password']") }
-    get logInButtonPasswordScreen() { return cy.get("button[class='inline-flex items-center justify-center whitespace-nowrap text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-black hover:bg-white px-4 py-2 rounded-3xl my-5 h-[51px] w-[440px]']") }
+    get loginButtonPasswordScreen() { return cy.get("button[class='inline-flex items-center justify-center whitespace-nowrap text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-black hover:bg-white px-4 py-2 rounded-3xl my-5 h-[51px] w-[440px]']") }
+    get signupButtonEmailScreen() { return cy.get("span[class='mx-1 hover:underline hover:cursor-pointer text-secondary']") }
+    get signupButtonPasswordScreen() { return cy.get("span[class='mx-1 hover:underline hover:cursor-pointer text-secondary']")}
+    get googleSigninButton() { return cy.get("body > div:nth-child(5) > div:nth-child(2) > div:nth-child(1) > button:nth-child(2)") }
+    get forgotPasswordButtonEmailScreen() { return cy.get("button[class='inline-flex items-center justify-center whitespace-nowrap text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ring-[0.5px] ring-primary bg-transparent hover:bg-light-gray px-4 py-2 rounded-3xl text-white my-5 h-[40px] w-[300px]']") }
+    get forgotPasswordButtonPasswordScreen() { return cy.get("span[class='w-[440px] hover:underline hover:cursor-pointer font-light text-secondary']") }
 }
 
 export default new LoginPagePo()
