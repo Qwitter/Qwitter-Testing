8d32c13648e2852d1599c29b1938bd546a5edd70
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2023-11-07 20:21:21 +0200

    test(login): proceed to password screen
    Added test cases to login page:
    - proceeds to password screen with valid email

--- Web/cypress/e2e/login/login.cy.js
+++ Web/cypress/e2e/login/login.cy.js
@@ -9,12 +9,21 @@ describe('login page', () => {
 
     beforeEach('navigate to login page and input field should be empty', () => {
         cy.visit('')
-        LoginPagePo.emailInputField.should('be.empty')
+        LoginPagePo.emailInputFieldEmailScreen.should('be.empty')
         LoginPagePo.nextButtonEmailScreen.should('be.disabled')
     })
 
     it('requires email', () => {
-        LoginPagePo.emailInputField.type(data.loginPage.dummyEmail)
+        LoginPagePo.emailInputFieldEmailScreen.type(data.loginPage.dummyEmail)
         LoginPagePo.nextButtonEmailScreen.should('be.enabled')
+    })
+
+    it('proceeds to password screen with valid email', () => {
+        LoginPagePo.emailInputFieldEmailScreen.type(data.loginPage.dummyEmail)
+        LoginPagePo.nextButtonEmailScreen.click()
+        LoginPagePo.titlePasswordScreen.should('contain.text', 'Enter your password')
+        LoginPagePo.emailInputFieldPasswordScreen.should('have.value', data.loginPage.dummyEmail)
+        LoginPagePo.passwordInputFieldPasswordScreen.should('be.empty')
+        LoginPagePo.logInButtonPasswordScreen.should('be.disabled')
     })
 })
--- Web/cypress/support/page-objects/LoginPagePo.js
+++ Web/cypress/support/page-objects/LoginPagePo.js
@@ -1,6 +1,10 @@
 class LoginPagePo {
-    get emailInputField() { return cy.get("input[name='email']") }
+    get emailInputFieldEmailScreen() { return cy.get("input[name='email']") }
     get nextButtonEmailScreen() { return cy.get("body > div:nth-child(5) > div:nth-child(2) > div:nth-child(1) > button:nth-child(7)") }
+    get titlePasswordScreen() { return cy.get("p[class='my-5 text-3xl font-bold break-words text-start h-[40px] w-[440px]']") }
+    get emailInputFieldPasswordScreen() { return cy.get("body > div:nth-child(5) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) input") }
+    get passwordInputFieldPasswordScreen() { return cy.get("input[name='password']") }
+    get logInButtonPasswordScreen() { return cy.get("button[class='inline-flex items-center justify-center whitespace-nowrap text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-black hover:bg-white px-4 py-2 rounded-3xl my-5 h-[51px] w-[440px]']") }
 }
 
 export default new LoginPagePo()
