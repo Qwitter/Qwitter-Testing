0ffdb42f5d2ea5eaa1edfa7a72a99a04c9334ab9
[Marwan Emad] <[marwanemad910@gmail.com]>
2023-11-14 15:08:12 +0200

    test(signup): signup form login page
    Add new tests to sign up form login page link and
    sign in with the new email and password

---
+++ Web/cypress/e2e/signup/sginup.cy.js
@@ -0,0 +1,36 @@
+import SignUpPage from '../../support/page-objects/signup'
+import { goToStep } from '../../utils/signup/signup'
+const data = require('../../fixtures/signup-data.json')
+
+describe('Sign up form anther endpoints', () => {
+    beforeEach('open login page', () => {
+        cy.clearCookies()
+        cy.clearLocalStorage()
+        cy.visit('')
+    })
+
+    it('sign up from singup link in login page', () => {
+        SignUpPage.sginIn.click()
+        SignUpPage.signupLink.click()
+        SignUpPage.createAccount.click()
+        
+        let testEmail = `test.${new Date().getTime()}@${Cypress.env("MAILISK_NAMESPACE")}.mailisk.net`;
+        goToStep(6, testEmail, true)
+        cy.url().should('include', 'profile')
+        
+        
+        cy.clearCookies()
+        cy.clearLocalStorage()
+        cy.visit('')
+        // here i should use a module of login but the teammate didn't push it yet
+        SignUpPage.sginIn.click()
+        SignUpPage.emailField.type(testEmail)
+        SignUpPage.nextButton.click()
+        SignUpPage.passwordField.type(data.strongPassword)
+        SignUpPage.logIn.click()
+
+        cy.url().should('include', '/account')
+    })
+
+    
+})
