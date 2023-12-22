3bf3f0859db4e4b478576758dec6e23c6ef473e6
[Marwan Emad] <[marwanemad910@gmail.com]>
2023-12-22 22:25:53 +0200

    test(search): add tests for search

---
+++ Mobile/test/page-objects/search.js
@@ -0,0 +1,30 @@
+class SearchPage{
+    async searchTab(){
+        return await $('~Tab 2 of 5')
+    }
+    async searchBar(){
+        return await $('~Search Qwitter')
+    }
+    async search(){
+        return await $('//android.widget.EditText[@index="1"]')
+    }
+    async clearSearch(){
+        const elements = await $$('//android.widget.Button')
+        return elements[1]
+    }
+    async firstUser(){
+        const elements = await $$('//android.view.View[@index="0"]')
+        return elements[6] 
+    }
+    async postsTab(){
+        return await $('~Posts Tab 1 of 4')
+    }
+    
+    async notExistingUserMessage(){
+        return await $('~Searching for the result')
+    }
+    
+    
+}
+
+module.exports = new SearchPage();
---
+++ Mobile/test/specs/search/search.e2e.js
@@ -0,0 +1,64 @@
+const { browser } = require('@wdio/globals')
+const commands = require('../../../commands.js')
+const data  = require('../../fixtures/login-data.json')
+const SearchPage = require('../../page-objects/search.js')
+const loginUtils = require('../../utils/login.js')
+const searchUtils = require('../../utils/search.js')
+
+
+describe('Search test suite', ()=>{
+    const searchKeyword = 'ma'
+    const notExistingUser = 'notExistingUserIsComming'
+    before( async () => {
+        await loginUtils.login(data.user.email, data.user.password)
+    })
+
+    afterEach(async () => {
+        await commands.restartApp()
+        await browser.pause(3500);
+    })
+
+    beforeEach( async ()=>{
+        // open email settings
+        const searchTab = await SearchPage.searchTab()
+        await searchTab.click()
+        const searchBar = await SearchPage.searchBar()
+        await searchBar.click()
+    })
+
+    it('search bar should be visible', async () => {
+        const search = await SearchPage.search()
+        expect(await search.isDisplayed()).toBe(true)
+    })
+
+    it('clear search keyword', async () => {
+        await searchUtils.searchFor("Clear Search")
+        const clear = await SearchPage.clearSearch()
+        await clear.click()
+        const search = await SearchPage.search()
+        expect(search).toHaveText('')
+    })
+
+    it('results empty when input empty', async () => {
+        const noUsersMessage = await SearchPage.notExistingUserMessage()
+        await expect(await noUsersMessage.isDisplayed()).toBe(true)
+    })
+    
+    it('search for non-existing user', async () => {
+        await searchUtils.searchFor(notExistingUser)
+        const noUsersMessage = await SearchPage.notExistingUserMessage()
+        await expect(await noUsersMessage.isDisplayed()).toBe(true)
+    })
+
+    it('search for existing user', async () => {
+        await searchUtils.searchFor(searchKeyword)
+        const firstUser = await SearchPage.firstUser()
+        if(firstUser){
+            await firstUser.click()
+            await browser.pause(1000)
+            const postsTab = await SearchPage.postsTab()
+            expect(await postsTab.isDisplayed()).toBe(true)
+        }
+    })
+
+})
